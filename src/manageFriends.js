b60c8089ab435bc36784235bde6b8e257840601c
Kyle <khartson2017@gmail.com>
2022-09-13 16:41:57 -0400

    commit tree 7744c80b15c8686ff7eb254ceba37ad30b0b705f

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,19 @@
 export function manageFriends(state, action) {
   // your code here
+  switch(action.type) {
+    case 'friends/add':
+      {
+        const newFriends = state["friends"]
+        newFriends.push({"hometown":action.payload.hometown, "id": action.payload.id, "name": action.payload.name })
+        return { friends: newFriends}
+      }
+    case 'friends/remove':
+      {
+        const newFriends = state["friends"].filter(friend => friend.id !== action.payload)
+        return { friends: newFriends}
+      }
+    default:
+      return state
+
+  }
 }
